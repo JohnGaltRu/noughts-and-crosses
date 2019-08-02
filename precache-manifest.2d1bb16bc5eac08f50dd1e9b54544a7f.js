9c6d013d122d1ac75bbf62cf6191885caea06764
ntts2 <ntts2@list.ru>
2019-08-02 16:43:20 +0300

    Updates

---
+++ precache-manifest.2d1bb16bc5eac08f50dd1e9b54544a7f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0fe90615c768a2bf19d174304a613dd5",
+    "url": "/noughts-and-crosses/index.html"
+  },
+  {
+    "revision": "376d9b22a6519e3adca7",
+    "url": "/noughts-and-crosses/static/css/main.fba0cf7b.chunk.css"
+  },
+  {
+    "revision": "9a5c0cc5b7b5942488d9",
+    "url": "/noughts-and-crosses/static/js/2.56cb80cd.chunk.js"
+  },
+  {
+    "revision": "376d9b22a6519e3adca7",
+    "url": "/noughts-and-crosses/static/js/main.36c4e04b.chunk.js"
+  },
+  {
+    "revision": "43c83ab49697412a9d81",
+    "url": "/noughts-and-crosses/static/js/runtime~main.2782f76b.js"
+  },
+  {
+    "revision": "c62aded729bf7146d491275e5019d7fc",
+    "url": "/noughts-and-crosses/static/media/Gilroy-Light.c62aded7.otf"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/noughts-and-crosses/precache-manifest.2d1bb16bc5eac08f50dd1e9b54544a7f.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/noughts-and-crosses/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+});
